2951cfc7259c6027f29881048c2a0c148443e0d0
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-01-20 22:52:45 +0900

    #0.4 import kakao Test

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import kakao from "./kakao";
 
 const server = express();
+
+server.get("/api/kakao", (req, res) => {
+  res.json(kakao);
+});
 
 server.listen(3000, () => {
   console.log("The server is running");
---
+++ kakao.js
@@ -0,0 +1,14 @@
+const kakao = [
+  {
+    id: 0,
+    name: "yong",
+    email: "ysy1644@naver.com",
+  },
+  {
+    id: 1,
+    name: "ryong9rrr",
+    email: "tkddbs1644@gmail.com",
+  },
+];
+
+export default kakao;
