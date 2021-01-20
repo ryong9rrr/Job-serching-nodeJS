e0b0075419cc6cd55b8f1178d906f227d386b042
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-01-21 00:51:17 +0900

    #1.0 graphql and axios

--- kakao.js
+++ kakao.js
@@ -1,14 +1,14 @@
-const kakao = [
-  {
-    id: 0,
-    name: "yong",
-    email: "ysy1644@naver.com",
-  },
-  {
-    id: 1,
-    name: "ryong9rrr",
-    email: "tkddbs1644@gmail.com",
-  },
-];
+const axios = require("axios");
 
-export default kakao;
+const getKakao = async () => {
+  try {
+    const response = await axios.get(
+      "https://careers.kakao.com/2021-developer"
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getKakao();
