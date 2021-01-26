6f4e5244ce4ca0d235bc4b463169303c82f67191
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-01-26 21:38:22 +0900

    #3.0 Creating kakao_all.js

--- kakao_all.js
+++ kakao_all.js
@@ -17,8 +17,29 @@ getHtml()
   .then((pages) => getJobs(pages));
 
 const getJobs = (pages) => {
-  for (var i = 1; i <= pages; i++) {
-    const response = axios.get(`${kakao_URL}page=${i}`);
-    return response;
+  //const response = axios.get(`${kakao_URL}page=1`);
+  try {
+    return axios.get(`${kakao_URL}page=1`);
+  } catch (error) {
+    console.error(error);
   }
 };
+
+getJobs().then((page) => {
+  const jobList = [];
+  const $ = cheerio.load(page.data);
+  const $getJobs = $("ul.list_jobs").children("li");
+
+  $getJobs.each(function (i, elem) {
+    jobList[i] = {
+      id: i,
+      title: $(this).find("h4.tit_jobs").text(),
+    };
+  });
+  console.log(jobList);
+});
+//.then((res) => console.log(res));
+
+// 이런식으로 리스트를 만들면 되는데... 내일은 링크타고 들어가서 파싱하기
+// 링크타고 들어가서 리스트 다시만들기
+// key 값도 줘야하나?
